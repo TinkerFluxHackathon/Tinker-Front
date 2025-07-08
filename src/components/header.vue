301e26d4f0317eaf8f0666eca9d09060ad432c97
joaogmotaa <joaogabrielceeduc2020@gmail.com>
2025-07-08 11:54:54 -0300

    CHORE: terminar header

---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<template>
+  <header class="header">
+    <nav class="cabecalho">
+    <span class="logo-text">tinkerflux</span>
+      <a href="#home">Home</a>
+      <a href="#sobre">Sobre</a>
+      <a href="#produtos">Produtos</a>
+      <a href="#contato">Contato</a>
+    </nav>
+  </header>
+
+</template>
+
+<style scoped>
+header {
+  background-color: #4584d3;
+  color: white;
+  padding: 3rem;
+}
+header a {
+  color: white;
+  text-decoration: none;
+  margin-right: 6rem;
+  font-size: 1.5rem;
+}
+header span.logo-text {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-right: 60rem;
+  margin-left: rem;
+}
+
+
+</style>
