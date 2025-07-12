3be100cd7a42a156043b1b8c194c05641e4a2829
Arthur15Goulart <arthurgoulartteixeirafabio48@gmail.com>
2025-07-12 20:24:43 -0300

    CHORE: terminar o css do login e suas rotas

---
+++ src/views/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+<main>
+    <section class="login-section">
+        <h1>Login</h1>
+        <form>
+            <div>
+                <label for="email">Seu email</label> 
+                <input type="text" id="email" name="email" required>
+            </div>
+            <div>
+                <label for="password">Sua Senha</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button type="submit" v-on="click">Continuar</button>
+        </form>
+    </section>
+</main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<main>
+    <section class="login-section">
+        <h1>Login</h1>
+        <form>
+            <div>
+                <label for="email">Seu email</label> 
+                <input type="text" id="email" name="email" required>
+            </div>
+            <div>
+                <label for="password">Sua Senha</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <button @click="enviar">Continuar</button>
+        </form>
+    </section>
+</main>
+</template>
+
+<script setup>
+function enviar(event) {
+    event.preventDefault();
+
+}
+</script>
+
+<style scoped>
+
+</style>
