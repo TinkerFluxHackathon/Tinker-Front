999799e857150d94746abdd9887e66fd9e912a4e
Arthur15Goulart <arthurgoulartteixeirafabio48@gmail.comgit config --global user.email arthurgoulartteixeirafabio48@gmail.com>
2025-08-14 11:09:21 -0300

    CHORE: Terminar o css do login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,23 @@
 <template>
 <main>
     <section class="login-section">
-        <h1>Login</h1>
         <form>
+          <h1>Login</h1>
             <div class="labels-inputs-login">
                 <label for="email/name">Seu nome de Usuário / Seu email  </label> <br>
-                <input type="text" id="email" name="email/name" required> <br>
+                <input type="text" id="email" name="email/name" placeholder="emaildecontato@gmail.com" required> <br>
 
                 <label for="password">Sua Senha  </label> <br>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password" placeholder="42069" required>
             </div>
 
             <nav>
-                <router-link to="/home">
-
-                  <div class="continue-button">
+                <router-link to="/home" class="button-to-home">
                     Continuar
-                  </div>
-
             </router-link>
-          
+
             </nav>
-               
-         
+
         </form>
 
     </section>
@@ -35,35 +30,57 @@
 
 <style scoped>
 
-.labels-inputs-login {
-    font-weight: bold;
+form {
+  margin: auto;
+}
+
+label {
+    font-weight: 370;
     color: #FFFFFF;
-    font-size: 10px;
+    font-size: 11px;
+}
+
+.labels-inputs-login {
     line-height: 1.4vw;
-    
+
+}
+
+input::placeholder {
+font-weight: 400;
+color: #FFFFFF;
 }
 
 input{
-    width: 15vw;
+    width: 17vw;
     border: 1px solid #ccc;
     border-radius: 1px;
     background-color: #1C2532;
+    padding: 2.5px;
 }
 
 section.login-section {
-    max-width: 400px;
     margin: auto;
     padding: 20px;
     border: 2px solid #ccc;
     border-radius: 8px;
     background-color: #1C2532;
+    width: 33.4vw;
+    height: 26vw;
+
 }
 
 section.login-section h1 {
     text-align: center;
     font-size: 2vw;
     font-weight: 500;
-    color: #F5F7FA
+    color: #F5F7FA;
+}
+
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 77vh;
 }
 
 
