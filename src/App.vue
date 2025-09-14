263fa4b8655793f134eba4c944a9ca7a354ae13f
doginvi <victordesousaestudo@gmail.com>
2025-09-14 18:12:16 -0300

    FEAT: Criação da página sobre nós

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Header from './components/Header.vue';
 import MainContent from './components/MainContent.vue';
 import Highlights from './components/Highlights.vue';
 import Products from './components/Products.vue';
+import AboutComponent from './components/AboutComponent.vue';
 import Footer from './components/Footer.vue';
 
 </script>
@@ -11,9 +12,12 @@ import Footer from './components/Footer.vue';
 <template>
   <div id="app">
     <Header />
-    <MainContent />
-    <Highlights />
-    <Products />
+    <!--
+     <MainContent />
+     <Highlights />
+     <Products /> 
+     -->
+    <AboutComponent />
     <Footer />
   </div>
 </template>
