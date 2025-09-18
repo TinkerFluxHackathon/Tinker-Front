b414be42a74a30f94ae8fce02aeb2f4bfcaf1e88
doginvi <victordesousaestudo@gmail.com>
2025-09-18 07:26:06 -0300

    CHORE: Iniciando a: Integração da ia

--- src/views/IA.vue
+++ src/views/IA.vue
@@ -1,5 +1,14 @@
 <script setup>
+  import { ref } from 'vue';
+  import { VMarkdownView} from 'vue3-markdown'
+  import 'vue3-markdown/dist/vue3-markdown.css'
 
+  const API_KEY = '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'
+  const MODEL_ID = 'deepseek/deepseek-r1:free'
+
+  const messages = ref([])
+  const userInput = ref('')
+  
 </script>
 
 <template>
